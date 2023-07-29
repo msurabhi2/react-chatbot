00566e2d7c79eea76c70b2bd6bfc3696ae37ad90
Surabi <mishra.surabhi2@gmail.com>
2023-07-30 00:38:05 +0530

    remove space

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
+    console.log("inside prod")
     // we are in production - return the prod set of keys
     module.exports = require('./prod');
 } else {
