333075057189c6d09a788af163fe4dc3ee6158ce
Surabi <mishra.surabhi2@gmail.com>
2023-08-08 12:06:55 +0530

    cross origin header added

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 app.get('/', (req, res) => {
     res.send({'hello': 'there'});
 })
